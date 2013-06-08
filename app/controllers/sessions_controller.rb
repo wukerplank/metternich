e4bf8d0619c7f32a673156aa014a6204bffb55bb
Christoph Edthofer <wukerplank@edthofer.at>
2013-06-09 00:18:29 +0200

    create proper user objects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
   
   skip_before_filter :user_required
-  skip_before_filter :verify_authenticity_token # FIXME workaround => should not be needed
+  skip_before_filter :verify_authenticity_token # FIXME workaround => should not be necessary, seems to be a bug in Rails 4.beta
   
   def create
-    unless (Rails.env.production? && @user = User.find_by_twitter_uid(auth_hash['uid'])) || @user = User.find_by_twitter_uid(auth_hash['name'])
+    unless (Rails.env.production? && @user = User.find_by_twitter_uid(auth_hash['uid'])) || @user = User.find_by_twitter_uid(user_info['name'])
       @user = User.create(
-        :nickname => (user_info['nickname'].present? ? user_info['nickname'] : user_info['name'])
+        :nickname     => (Rails.env.production? ? user_info['nickname'] : user_info['name']),
+        :twitter_uid  => (Rails.env.production? ? auth_hash['uid']      : user_info['name']),
+        :twitter_hash => auth_hash.to_yaml
       )
       @user.save
     end
@@ -23,10 +25,12 @@ class SessionsController < ApplicationController
 private
   
   def auth_hash
+    logger.info request.env['omniauth.auth'].inspect
     request.env['omniauth.auth']
   end
   
   def user_info
+    logger.info (auth_hash['user_info'] || auth_hash['info']).inspect
     auth_hash['user_info'] || auth_hash['info']
   end
   
